90cd5f7f73ee71a5e95f6b2ac6745313508b035e
Christian Jauvin <cjauvin@gmail.com>
2012-11-09 16:59:23 -0500

    soffice server script

---
+++ start_soffice_server.sh
@@ -0,0 +1,5 @@
+# OO 3.2
+sudo su www-data -c 'soffice -headless -accept="socket,host=localhost,port=2002;urp;" &'
+
+# LO 3.5
+#sudo su www-data -c 'soffice --headless --accept="socket,host=localhost,port=2002;urp;" &'
