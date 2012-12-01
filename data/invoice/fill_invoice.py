c674958d0727724974c7e13c8a348973a6be1b13
Christian Jauvin <cjauvin@gmail.com>
2012-12-01 17:44:27 -0500

    -invoice +templates

--- data/invoice/fill_invoice.py
+++
@@ -1,8 +0,0 @@
-from appy.pod.renderer import Renderer
-
-items = [('12', 'ROU724 Valle D\'Oro Rosso .. 2009', '750 ml', '1.59$', '19.08$'),
-         ('12', 'ROU724 Valle D\'Oro Rosso .. 2009', '750 ml', '1.59$', '19.08$'),
-         ('12', 'ROU724 Valle D\'Oro Rosso .. 2009', '750 ml', '1.59$', '19.08$')]
-
-renderer = Renderer('vinum_invoice_tmpl.odt', globals(), '/tmp/vinum/invoice1.pdf', overwriteExisting=True)
-renderer.run()
